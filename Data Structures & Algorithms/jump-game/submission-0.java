caa5d99fcdcf5eb66debe9fd3fb8333f8ddebaaf
Utpal Lochan Das <111297518+Utpal07Dev@users.noreply.github.com>
2026-07-25 18:38:28 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for(int i = 0;i<nums.length;i++){
+                int dist = nums[i];
+
+                if(maxReach<i)return false;
+                if((i+nums[i])>maxReach)maxReach = i+nums[i];
+                if (maxReach >= nums.length - 1) {
+                return true;
+            }
+
+
+
+        }
+        return true;
+    }
+}
