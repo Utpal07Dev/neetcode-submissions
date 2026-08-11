627bd215833a4e53a09fd83d1c19701ad71588f2
Utpal Lochan Das <111297518+Utpal07Dev@users.noreply.github.com>
2026-08-11 10:45:00 +0530

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int low = 0;
+        int n = nums.length;
+        int high = n-1;
+        int ans1 = -1;
+        int ans2 = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]>=target){
+                 if (nums[mid] == target) ans1 = mid; 
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+    low = 0;
+    high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]<=target){
+                if (nums[mid] == target) ans2 = mid;
+                low = mid+1;
+            }
+            else high = mid-1;
+        }
+        return new int[] {ans1,ans2};
+
+    }
+}
